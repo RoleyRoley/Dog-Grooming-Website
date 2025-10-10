6628d11595a28ad2daf46722478717d63524fe63
RoleyRoley <sonnyjohnjeffreys@gmail.com>
2025-10-10 12:01:57 +0100

    Added scroll to top feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import AppRoutes from './Routes';
+import ScrollToTop from './components/shared/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
         <AppRoutes />
       </Layout>
